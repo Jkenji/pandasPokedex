2c51edeafa0834141717f5986ba9d4f04fb02bbf
Enzo Kikuchi <127358329+Jkenji@users.noreply.github.com>
2024-08-28 21:20:20 -0300

    atualizando projeto

--- Main.py
+++ Main.py
@@ -13,15 +13,37 @@ df = pd.read_csv('pokemon_data.csv')
 # for index, row in df.iterrows():
 #    print(index, row)
 
-# print(df.loc[(df['Type 1'] == 'Fire') | (df['Type 2'] == 'Fire')])
+#print(df.loc[(df['Type 1'] == 'Fire') & (df['Type 2'] == 'Ghost')])
 
 # df.sort_values(by=['Type', 'HP'], ascending=[1, 0)
 
 # df = df.drop(columns=['Total'])
 
-df['Total'] = df.iloc[:, 4:9].sum(axis=1)
+#df['Total'] = df.iloc[:, 4:9].sum(axis=1)
 
-print(df.head(5))
+#poisontypes = df.loc[(df['Type 1'] == 'Fire') & (df['Type 2'] == 'Poison') & (df['HP'] >70)]
+#print(poisontypes.iloc[:800])
+
+#cols = list(df.columns)
+#df = df[cols[0:4] + [cols[-1]] + cols[4:12]]
+#df.head(15)
+
+#df.to_csv('new_df.csv', index = False)
+
+#new_df = df.loc[(df['Type 1'] == 'Grass') & (df['Type 2'] == 'Poison') & (df['HP'] >70)]
+#new_df = new_df.reset_index(drop = True)
+#print(new_df)
+
+#no_megas = df.loc[~df['Name'].str.contains('Mega')]
+#print(no_megas)
+#no_megas.to_csv('no_megas.csv')
+
+import re
+
+#df.loc[df['Type 1'].str.contains("Fire|Grass", flags = re.I, regex = True)]
+
+nameFilt = df.loc[df['Name'].str.contains("^pi[a-z]*", flags = re.I, regex = True)]
+print(nameFilt)
 
 
 
